4f3c5785b7ecf5919a7e903e63509c7d7173d4a5
sameera chathuranga <chathuranga@empite.com>
2018-05-08 17:21:02 +0530

    distribution pod added for appcenter

--- ios/ReactNativeAppCenter/AppDelegate.m
+++ ios/ReactNativeAppCenter/AppDelegate.m
@@ -9,6 +9,8 @@
 #import <AppCenterReactNativeCrashes/AppCenterReactNativeCrashes.h>
 #import <AppCenterReactNativeAnalytics/AppCenterReactNativeAnalytics.h>
 #import <AppCenterReactNative/AppCenterReactNative.h>
+@import AppCenter;
+@import AppCenterDistribute;
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -23,8 +25,12 @@
 
   [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:true];  // Initialize AppCenter analytics
 
+  [MSAppCenter start:@"a70bd2ab-0e3b-48d3-833b-e9fa44a03974" withServices:@[[MSDistribute class]]];
+  
   [AppCenterReactNative register];  // Initialize AppCenter 
 
+  [MSDistribute setDelegate:self];
+  
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
@@ -38,6 +44,33 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  return YES;
+}
+  
+- (BOOL)distribute:(MSDistribute *)distribute releaseAvailableWithDetails:(MSReleaseDetails *)details {
+  
+  // Your code to present your UI to the user, e.g. an UIAlertController.
+  UIAlertController *alertController = [UIAlertController
+                                        alertControllerWithTitle:@"Update available."
+                                        message:@"Do you want to update?"
+                                        preferredStyle:UIAlertControllerStyleAlert];
+  
+  [alertController
+   addAction:[UIAlertAction actionWithTitle:@"Update"
+                                      style:UIAlertActionStyleCancel
+                                    handler:^(UIAlertAction *action) {
+                                      [MSDistribute notifyUpdateAction:MSUpdateActionUpdate];
+                                    }]];
+  
+  [alertController
+   addAction:[UIAlertAction actionWithTitle:@"Postpone"
+                                      style:UIAlertActionStyleDefault
+                                    handler:^(UIAlertAction *action) {
+                                      [MSDistribute notifyUpdateAction:MSUpdateActionPostpone];
+                                    }]];
+  
+  // Show the alert controller.
+  [self.window.rootViewController presentViewController:alertController animated:YES completion:nil];
   return YES;
 }
 
